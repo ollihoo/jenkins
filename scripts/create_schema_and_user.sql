084abb18836e4d359a0b804bca214789be76dd7d
Oliver Hoogvliet <oliver.hoogvliet@codecentric.de>
2017-03-07 10:31:45 +0100

    remove scripts folder

--- scripts/create_schema_and_user.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE "versioning";
-
-CREATE USER "foobar"@"localhost" IDENTIFIED BY "geheim2345!";
-
-USE versioning;
-
-CREATE TABLE VERSION (id long sequence, description varchar(200), cdate DATETIME, mdate DATETIME);
-
--- scripts/install_psql.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ssh -p 22 vagrant@localhost
-
-sudo yum install postgresql -y
-
-sudo systemctl enable postgresql.service
-sudo systemctl start postgresql.service
-
