bb82389c34abc85288bb5376cfb957e72df9f394
Oliver Hoogvliet <oliver.hoogvliet@codecentric.de>
2017-03-07 13:04:51 +0100

    postgres vagrant user gets jenkins key as authorized_key. Jenkins gets ansible; to distribute the public key, script must be adapted

--- generate_key_for_jenkins.sh
+++ generate_key_for_jenkins.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# generate ssh keys for jenkins
 ssh-keygen -b 1024 -t dsa -f hosts/roles/jenkins/files/id_rsa
+
+# the public key is also used in the postgres playbook
+cp hosts/roles/jenkins/files/id_rsa.pub hosts/roles/postgres/files/authorized_keys
